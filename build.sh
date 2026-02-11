2f31349cec9c5607d636c06ff9a6fb7cbaa71459
Daniel-Sinkin <danielsinkin97@gmail.com>
2026-02-11 19:53:51 +0100

    trying to get local svg compile pipeline to work

--- build.sh
+++ build.sh
@@ -3,10 +3,14 @@ set -euo pipefail
 
 mkdir -p build
 
+latexmk -C -outdir=build tex/main.tex >/dev/null 2>&1 || true
+
 latexmk \
+  -pdf \
   -xelatex \
   -shell-escape \
   -interaction=nonstopmode \
   -file-line-error \
+  -synctex=1 \
   -outdir=build \
-  main.tex
+  tex/main.tex
